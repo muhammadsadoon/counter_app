02799de339e3375c5803ad9c4bad689e61830c0f
sadoon <muhammadsadoonsohail786@gmail.com>
2025-06-20 12:47:39 +0500

    add hide and show button

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var display = document.getElementById("display");
+var increamentBtn = document.getElementById("btn_2");
 var number = 0;
-function increament(){
+function checkBtn() {
+    if(number>= 1){
+        increamentBtn.style.display = "block";
+    }else{
+        increamentBtn.style.display = "none";
+    }
+}
+function increament() {
     number++
     display.innerHTML = number;
+    checkBtn()
 }
-function decreament(){
-    if(number > 0){
+function decreament() {
+    if (number > 0) {
         number--
         display.innerHTML = number;
     }
+    checkBtn()
 }
+checkBtn()
